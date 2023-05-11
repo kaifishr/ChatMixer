bdd02a0bdba6f7def88e0faddc5f0042f09b63ec
Kai Fischer <33313471+kaifishr@users.noreply.github.com>
2023-05-11 21:10:03 +0200

    Add experimental displaced input-target-tuples.

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -39,37 +39,113 @@ class CharDataset(Dataset):
     def __len__(self):
         return len(self.data) - (self.input_sequence_length + self.output_sequence_length)
 
-    def __getitem__(self, idx):
+    def _get_sequence_connected(self, idx: int) -> tuple[list[int], list[int]]:
+        """Creates connected sequence.
+        
+        Args:
+            idx: Index to access string stored in 'data'
+
+        Returns:
+            Input and target tensors.
+        """
+        sequence_length = self.input_sequence_length + self.output_sequence_length
+        char_sequence = self.data[idx : idx + sequence_length]
+        idx_sequence = [self.char_to_index[char] for char in char_sequence]
+        idx_sequence_x = idx_sequence[: self.input_sequence_length]
+        idx_sequence_y = idx_sequence[self.input_sequence_length :]
+        return idx_sequence_x, idx_sequence_y
+
+    def _get_sequence_displaced(self, idx: int) -> tuple[list[int], list[int]]:
+        """Creates interleaved sequence.
+
+        Creates a sequence of interleaved inputs and targets that forces the
+        model to interpolate (predict missing tokens in the sequence) as 
+        well as extrapolate (predict token at end of the sequence). Here we
+        use slicing operations '[-2::-2][::-1]' for the input and 
+        '[-1::-2][::-1]' for the output to ensure that the to predicted output 
+        sequence always contains the sequence's last character.
+        
+        Args:
+            idx: Index to access string stored in 'data'
+
+        Returns:
+            Input and target tensors.
+        """
+        assert self.input_sequence_length % 2 == 0 
+        assert self.input_sequence_length == self.output_sequence_length
+        sequence_length = self.input_sequence_length
+        char_sequence = self.data[idx : idx + 2 * sequence_length]
+        idx_sequence = [self.char_to_index[char] for char in char_sequence]
+        idx_sequence_x = idx_sequence[-2::-2][::-1]
+        idx_sequence_y = idx_sequence[-1::-2][::-1]
+        return idx_sequence_x, idx_sequence_y
+
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
         """Extracts sequence of characters from data.
 
-        For data holding a sequence of characters
+        For 'data' holding a sequence of characters like
 
         data = "The quick brown Fox jumps"
 
-        idx=4 and input_sequence_length=8,
-        output_sequence_length=2 the following block
+        with idx = 4, input_sequence_length = 8, and
+        output_sequence_length = 2, the following sequence
         of characters are extracted from the data
         sequence
 
-        char_block = "quick brow"
+        char_sequence = "quick brow"
 
-        which is being encoded as a list of integers:
+        which is being encoded as a list of integers
 
-        encoded_block = [9, 1, 4, 8, 2, 5, 3, 7, 6, 0]
-                         q  u  i  c  k " " b  r  o, w
+        encoded_sequence = [9, 1, 4, 8, 2, 5, 3, 7, 6, 0]
+                            q  u  i  c  k " " b  r  o, w
 
-        From this list, the following input and target
-        is created:
+        From this list, the following input and target is created:
 
         x = [9, 1, 4, 8, 2, 5, 3, 7]
         y = [6, 0]
 
         Args:
             idx: Index to access string stored in data.
+
+        Returns:
+            Input and target tensors.
         """
-        sequence_length = self.input_sequence_length + self.output_sequence_length
-        char_sequence = self.data[idx : idx + sequence_length]
-        int_sequence = [self.char_to_index[char] for char in char_sequence]
-        x = torch.tensor(data=int_sequence[: self.input_sequence_length], dtype=torch.long)
-        y = torch.tensor(data=int_sequence[self.input_sequence_length :], dtype=torch.long)
+        # idx_sequence_x, idx_sequence_y = self._get_sequence_connected(idx=idx)
+        idx_sequence_x, idx_sequence_y = self._get_sequence_displaced(idx=idx)
+        x = torch.tensor(data=idx_sequence_x, dtype=torch.long)
+        y = torch.tensor(data=idx_sequence_y, dtype=torch.long)
         return x, y
+
+    # def __getitem__(self, idx):
+    #     """Extracts sequence of characters from data.
+
+    #     For 'data' holding a sequence of characters like
+
+    #     data = "The quick brown Fox jumps"
+
+    #     with idx = 4, input_sequence_length = 8, and
+    #     output_sequence_length = 2, the following sequence
+    #     of characters are extracted from the data
+    #     sequence
+
+    #     char_sequence = "quick brow"
+
+    #     which is being encoded as a list of integers
+
+    #     encoded_sequence = [9, 1, 4, 8, 2, 5, 3, 7, 6, 0]
+    #                         q  u  i  c  k " " b  r  o, w
+
+    #     From this list, the following input and target is created:
+
+    #     x = [9, 1, 4, 8, 2, 5, 3, 7]
+    #     y = [6, 0]
+
+    #     Args:
+    #         idx: Index to access string stored in data.
+    #     """
+    #     sequence_length = self.input_sequence_length + self.output_sequence_length
+    #     char_sequence = self.data[idx : idx + sequence_length]
+    #     int_sequence = [self.char_to_index[char] for char in char_sequence]
+    #     x = torch.tensor(data=int_sequence[: self.input_sequence_length], dtype=torch.long)
+    #     y = torch.tensor(data=int_sequence[self.input_sequence_length :], dtype=torch.long)
+    #     return x, y
